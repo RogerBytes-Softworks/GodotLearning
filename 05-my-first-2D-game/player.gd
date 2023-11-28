53c573d97a1b8a6e17f22e515bc5adb5d867a44d
Harry Richmond <rogerbytes@vivaldi.net>
2023-11-28 13:15:07 +0100

    no comment

--- 05-my-first-2D-game/player.gd
+++ 05-my-first-2D-game/player.gd
@@ -17,7 +17,7 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
-		velocity.x -= 1
+		velocity.y -= 1
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
